77d85931288a56f919df353a330b346f4e10a020
Bruno Almeida <bruno.fa.almeida@gmail.com>
2017-06-09 21:06:52 -0400

    Add start button to timer

--- Development/MyTime/MyTime/NewTaskViewController.swift
+++ Development/MyTime/MyTime/NewTaskViewController.swift
@@ -19,6 +19,8 @@ class NewTaskViewController: UIViewController {
     @IBOutlet weak var circleView: UIView!
     @IBOutlet weak var timerLabel: UILabel!
     
+    fileprivate var timer: Timer?
+    
     // The timer's start time (set a temporary value so it's non-nil)
     fileprivate var startTime: Date = Date()
     
@@ -35,7 +37,35 @@ class NewTaskViewController: UIViewController {
     
     override func viewDidLoad() {
         super.viewDidLoad()
-
+        
+        // Set the timer circle's size and colour
+        // rgb(135,206,250) - lightskyblue (http://www.rapidtables.com/web/color/blue-color.htm)
+        circleView.layer.cornerRadius = circleView.frame.size.width / 2
+        circleView.backgroundColor = UIColor(red: 135/255.0, green: 206/255.0, blue: 250/255.0, alpha: 0.5)
+        
+        cancelTimer()
+    }
+    
+    func cancelTimer() {
+        print(#function)
+        
+        // Back button in top left
+        navigationItem.leftBarButtonItem = nil
+        
+        // Start button in top right
+        let startButton = UIBarButtonItem(title: "Start", style: .done, target: self, action: #selector(startButtonTapped))
+        navigationItem.rightBarButtonItem = startButton
+        
+        // Stop the timer
+        timer?.invalidate()
+        timer = nil
+        
+        timerLabel.text = "0:00"
+    }
+    
+    func startTimer() {
+        print(#function)
+        
         // Cancel button in top left
         let cancelButton = UIBarButtonItem(barButtonSystemItem: .cancel, target: self, action: #selector(cancelButtonTapped))
         navigationItem.leftBarButtonItem = cancelButton
@@ -44,21 +74,20 @@ class NewTaskViewController: UIViewController {
         let doneButton = UIBarButtonItem(barButtonSystemItem: .done, target: self, action: #selector(doneButtonTapped))
         navigationItem.rightBarButtonItem = doneButton
         
-        // Set the timer circle's size and colour
-        // rgb(135,206,250) - lightskyblue (http://www.rapidtables.com/web/color/blue-color.htm)
-        circleView.layer.cornerRadius = circleView.frame.size.width / 2
-        circleView.backgroundColor = UIColor(red: 135/255.0, green: 206/255.0, blue: 250/255.0, alpha: 0.5)
-        
         // Initialize the timer's start time
         // (setting it now is more accurate than when the object is initialized)
         startTime = Date()
         
         // Fire a system timer every 0.1s, to update the task timer
-        _ = Timer.scheduledTimer(timeInterval: 0.1,
-                                 target: self,
-                                 selector: #selector(timerFired),
-                                 userInfo: nil,
-                                 repeats: true)
+        timer = Timer.scheduledTimer(timeInterval: 0.1,
+                                     target: self,
+                                     selector: #selector(timerFired),
+                                     userInfo: nil,
+                                     repeats: true)
+    }
+    
+    func startButtonTapped() {
+        startTimer()
     }
     
     /**
@@ -67,7 +96,8 @@ class NewTaskViewController: UIViewController {
      */
     func cancelButtonTapped() {
         print(#function)
-        _ = navigationController?.popViewController(animated: true)
+        cancelTimer()
+//        _ = navigationController?.popViewController(animated: true)
     }
     
     /**
