da8a4bd778d3de3bfece9689e6041c14ab4b6009
Bruno Almeida <bruno.fa.almeida@gmail.com>
2017-08-06 19:36:53 -0400

    Add transition to graph analysis view

---
+++ Development/MyTime/MyTime/GraphAnalysisViewController.swift
@@ -0,0 +1,35 @@
+//
+//  GraphAnalysisViewController.swift
+//  MyTime
+//
+//  Created by Bruno Almeida on 2017-08-06.
+//  Copyright © 2017 Bruno Almeida. All rights reserved.
+//
+
+import UIKit
+
+class GraphAnalysisViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destinationViewController.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
